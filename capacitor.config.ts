38a0aa369047d2164a3a6cff72a89fe2e0333bca
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-07 10:28:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taskr_app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
