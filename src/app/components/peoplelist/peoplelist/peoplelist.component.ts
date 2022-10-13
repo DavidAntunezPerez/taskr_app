770f83c7527a767e15232d75dbd29f0089b6f651
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-13 10:33:13 +0200

    Added peoplelist component

---
+++ src/app/components/peoplelist/peoplelist/peoplelist.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peoplelist',
+  templateUrl: './peoplelist.component.html',
+  styleUrls: ['./peoplelist.component.scss'],
+})
+export class PeoplelistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
