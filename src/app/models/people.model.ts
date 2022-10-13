88d1289e7b3c9b47cc91e3d6b1abeadbcefe36c5
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-13 08:38:12 +0200

    Added services and models

---
+++ src/app/models/people.model.ts
@@ -0,0 +1,7 @@
+export interface People{
+  id: number;
+  name: String;
+  surname: String;
+  sex?: String;
+  picture: String;
+}
--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { People} from '../models/people.model';
 @Injectable({
   providedIn: 'root',
 })
@@ -9,4 +9,32 @@ export class PeopleService {
   sex?: String;
   picture: String; // not yet showed
   constructor() {}
+  person:People[] = [
+    {
+      id: 0,
+      name: "Paquito",
+      surname:"Potentus",
+      picture:"",
+    },
+    {
+      id:1,
+      name:"Pepe",
+      surname:"Pérez",
+      sex:"Male",
+      picture:"",
+    },
+    {
+      id:2,
+      name:"Juana",
+      surname:"Lopez",
+      sex:"Female",
+      picture:"",
+    },
+    {
+      id:3,
+      name:"Juanjo",
+      surname:"Jimenez",
+      picture:"",
+    }
+  ]
 }
