74b288c6d1ae82b6218de1e70ea7ee640b8fe3ea
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-21 08:59:42 +0200

    Created components and service for assignments

---
+++ src/app/components/assigndetail/assigndetail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigndetail',
+  templateUrl: './assigndetail.component.html',
+  styleUrls: ['./assigndetail.component.scss'],
+})
+export class AssigndetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/assignlist/assignlist.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignlist',
+  templateUrl: './assignlist.component.html',
+  styleUrls: ['./assignlist.component.scss'],
+})
+export class AssignlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
