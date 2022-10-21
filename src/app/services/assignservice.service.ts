dff6ec5c4f8cb4f62935a5296ca293d5323dafdd
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-21 09:30:13 +0200

    Updated service assignment

--- src/app/services/assignservice.service.ts
+++ src/app/services/assignservice.service.ts
@@ -6,6 +6,66 @@ import { Assignment } from '../models/assign.model';
   providedIn: 'root'
 })
 export class AssignserviceService {
+  momentjs:any = moment;
+  
+  
+  private assgn:Assignment[] = [
+    {
+      id:1,
+      idPerson:1,
+      idTask:1,
+      dateCreation:this.momentjs().toISOString(),
+      dateTime:this.momentjs().add(1, 'days').toISOString(),
+    },
+    {
+        id:2,
+        idPerson:2,
+        idTask:2,
+        dateCreation:this.momentjs().toISOString(),
+        dateTime:this.momentjs().add(1, 'days').toISOString(),
+      }
+  ];
 
   constructor() { }
+
+  id:number = this.assgn.length+1;
+
+  // METHODS
+
+  getAssignments(){
+    return this.assgn;
+  }
+
+  getAssignmentById(id:number){
+    return this.assgn.find(a=>a.id==id);
+  }
+
+  getAssignmentsByTaskId(taskId:number):Assignment[]{
+    return this.assgn.filter(a=>a.idTask == taskId);
+  }
+
+  getAssignmentsByPersonId(idPerson:number):Assignment[]{
+    return this.assgn.filter(a=>a.idPerson == idPerson);
+  }
+
+  deleteAssignmentById(id:number){
+    this.assgn = this.assgn.filter(a=>a.id != id); 
+  }
+
+  addAssignment(assgn:Assignment){
+    assgn.id = this.id++;
+    this.assgn.push(assgn);
+  }
+
+  updateAssignment(assignment:Assignment){
+    var _assignment = this.assgn.find(a=>a.id==assignment.id);
+    if(_assignment){
+      _assignment.idTask = assignment.idTask ;
+      _assignment.idPerson = assignment.idPerson;
+      _assignment.dateCreation = assignment.dateCreation;
+      _assignment.dateTime = assignment.dateTime;
+    }
+    
+  }
+
 }
