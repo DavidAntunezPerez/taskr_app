85ce545d15f98011da243a0e007d799ad3896dbc
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-11-04 08:56:42 +0100

    Created taskshowlst

---
+++ src/app/components/taskshowlst/taskshowlst.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taskshowlst',
+  templateUrl: './taskshowlst.component.html',
+  styleUrls: ['./taskshowlst.component.scss'],
+})
+export class TaskshowlstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
