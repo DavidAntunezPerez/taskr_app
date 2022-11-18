0f4adafc22af2eb75b3c4bd798b37f7a3963ab23
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-11-18 11:17:16 +0100

    Translated view tasks page

--- src/app/pages/viewtasks/viewtasks.module.ts
+++ src/app/pages/viewtasks/viewtasks.module.ts
@@ -7,13 +7,23 @@ import { IonicModule } from '@ionic/angular';
 import { ViewtasksPageRoutingModule } from './viewtasks-routing.module';
 
 import { ViewtasksPage } from './viewtasks.page';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { createTranslateLoader } from 'src/app/utils/translate';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ViewtasksPageRoutingModule
+    ViewtasksPageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [ViewtasksPage]
 })
--- src/app/pages/viewtasks/viewtasks.page.ts
+++ src/app/pages/viewtasks/viewtasks.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-viewtasks',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewtasksPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private translateService: TranslateService) {
+  }
+  language: string = this.translateService.currentLang;
+  languageChange() {
+    this.translateService.use(this.language);
+  }
   ngOnInit() {
   }
 
